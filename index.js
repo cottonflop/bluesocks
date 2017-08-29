a0fec73d30528c9a83363a7aab6fd06491211c4d
Betsy Fox <betsy.fox@sony.com>
2017-08-29 14:21:30 -0700

    better undefined token handling?

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ let token = function(type, data, src, row, col, mode) {
 	}
 }
 
+
 let pos2d = function(s) {
 	lines = s.split(/(?=\n)/);
 	row = lines.length;
 	col = lines[lines.length - 1].length + 1
 	return [row, col];
 }
+
 
 let rule = function(name, regex) {
 	return { name: name, regex: regex };
@@ -33,14 +35,19 @@ var lexer = function*(s, src, rules, mode = "default") {
 	let newmode = undefined;
 
 	for (let i=0; i < s.length; i+=adv) {
+		[row, col] = pos2d(s.substr(0, i));
 		let match = -1;
 		let rule = rules[mode].find((x) => (s.substr(i).search(x.regex) == 0));
-		let found = rule.regex.exec(s.substr(i));
-		if (found == null) return "Something extremely weird happened.";
-		[row, col] = pos2d(s.substr(0, i));
-		newmode = yield token(rule.name, found[0], src, row, col, mode);
+		if (rule == undefined)  {
+			adv = 1;
+			newmode = yield token("UNDEFINED", s.substr(i, 1), src, row, col, mode); //throw `No matching rules found for mode: "${mode}" and input: "${s.substr(i, 1)}" in ${src} (${row}, ${col})`;
+		} else {
+			let found = rule.regex.exec(s.substr(i));
+			newmode = yield token(rule.name, found[0], src, row, col, mode);
+			adv = found[0].length;
+		}
 		mode = (newmode !== undefined) ? newmode : mode;
-		adv = found[0].length;
+		if (!rules.hasOwnProperty(mode)) throw `Undefined mode specified: "${mode}"`;
 	}
 }
 
