d120d9807b834039d6b9714ed5ca5d1b4a4bd8ae
Betsy Fox <cottonflop@gmail.com>
2017-09-08 09:18:45 -0700

    context change behavior moved to lexer/rules

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let token = function(type, data, src, row, col, mode) {
+let token = function(type, data, src, row, col) {
 	return {
 		type: type,
 		data: data,
 		src: src,
 		row: row,
 		col: col,
-		mode: mode
+		context: context
 	}
 }
 
+let context = [];
 
 let pos2d = function(s) {
 	lines = s.split(/(?=\n)/);
@@ -18,35 +19,52 @@ let pos2d = function(s) {
 }
 
 
-let rule = function(name, regex) {
-	return { name: name, regex: regex };
+let rule = function(name, regex, context) {
+	return { name: name, regex: regex, context: context };
+}
+
+
+let change_context = function(s) {
+	if (!s) return true;
+	switch(s[0]) {
+		case ">": //push
+			return (context.push(s.substr(1)) == s.substr(1));
+		case "<": //pop
+			return (context.pop() == s.substr(1));
+		default: //no.
+			console.log(`Unknown context switch encountered: "${s}"`);
+			return false;
+	}
+}
+
+let get_context = function() {
+	return context.length > 0 ? context.slice(-1)[0] : "default";
 }
 
 
 var lexer = function*(s, src, rules, mode = "default") {
+	context = [];
 	if (Array.isArray(rules)) rules = { default: rules };
 	if (s === undefined) return "No string provided!";
 	if (src === undefined) return "No source provided!";
 	if (rules === undefined) rules = { default: [rule("CHARACTER", /./)] };
-	let pos = 0;
-	let row = 1;
-	let col = 1;
-	let adv = 1;
-	let newmode = undefined;
+	let pos=0, row=1, col=1, data="";
+	let new_context = undefined;
 
-	for (let i=0; i < s.length; i+=adv) {
+	for (let i=0; i < s.length; i+=data.length) {
 		[row, col] = pos2d(s.substr(0, i));
-		let match = -1;
-		let rule = rules[mode].find((x) => (s.substr(i).search(x.regex) == 0));
+		// let match = -1;
+		console.log(`We're currently in context: "${get_context()}"`)
+		let rule = rules[get_context()].find((x) => (s.substr(i).search(x.regex) == 0));
 		if (rule == undefined)  {
-			adv = 1;
-			newmode = yield token("UNDEFINED", s.substr(i, 1), src, row, col, mode); //throw `No matching rules found for mode: "${mode}" and input: "${s.substr(i, 1)}" in ${src} (${row}, ${col})`;
+			data = s.substr(i, 1);
+			rule = {name: "UNDEFINED"};
 		} else {
-			let found = rule.regex.exec(s.substr(i));
-			newmode = yield token(rule.name, found[0], src, row, col, mode);
-			adv = found[0].length;
+			change_context(rule.context);
+			data = rule.regex.exec(s.substr(i))[0];
 		}
-		mode = (newmode !== undefined) ? newmode : mode;
+		// new_context = yield token(rule.name, data, src, row, col, mode) || rule.context; //throw `No matching rules found for mode: "${mode}" and input: "${s.substr(i, 1)}" in ${src} (${row}, ${col})`;
+		// if (new_context !== undefined) change_context(new_context);
 		if (!rules.hasOwnProperty(mode)) throw `Undefined mode specified: "${mode}"`;
 	}
 }
--- test.js
+++ test.js
@@ -2,12 +2,12 @@ var { lexer, rule } = require('.');
 
 let rules = {
 	default: [
-		rule("PERIOD", /\./),
-		rule("WORD", /[^\.]+/)
+		rule("PERIOD1", /\./, ">othermode"),
+		rule("WORD1", /[^\.]+/)
 	],
 	othermode: [
-		rule("PERIOD", /\./),
-		rule("WORD", /[^\.]+/)
+		rule("PERIOD2", /\./, "<othermode"),
+		rule("WORD2", /[^\.]+/)
 	]
 }
 
@@ -15,25 +15,15 @@ lex = lexer("This is a test. This is a test. This is a test.", "test", rules);
 
 let token = lex.next();
 while(!token.done) {
-	let mode_switch = undefined;
-	switch (token.value.mode) {
-		case "default":
-			switch (token.value.type) {
-				case "UNDEFINED":
-					console.log(`Error: Undefined token "${token.value.data}" in ${token.value.src}:${token.value.row}:${token.value.col}`);
-					mode_switch = "ERROR";
-					break;
-				case "PERIOD":
-					mode_switch = "othermode";
-				case "WORD":
-					console.log(`[MODE ${token.value.mode}] Found a ${token.value.type} "${token.value.data}" at (${token.value.row}, ${token.value.col})`);
-					break
-			}
+	switch (token.value.type) {
+		case "WORD1":
+		case "PERIOD1":
+			console.log("We're in the default context");
 			break;
-		case "othermode":
-			console.log(`[MODE ${token.value.mode}] Found "${token.value.data}" at (${token.value.row}, ${token.value.col})`);
-			if (token.value.type == "PERIOD") mode_switch = "default";
+		case "WORD2":
+		case "PERIOD2":
+			console.log("We're in the other context");
 			break;
-	}
-	token = lex.next(mode_switch);
+		}
+	token = lex.next();
 }
